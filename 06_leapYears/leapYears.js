f632fb5dc12042311e828661bad15ee31ff73f0e
Abigail <abigail.uduji@gmail.com>
2022-05-20 23:14:57 -0500

    Complete exercsise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,6 @@
 const leapYears = function(year) {
     let result;
-    if (year % 4 === 0 ) {
+    if (year % 4 === 0 && (!(year%100 === 0)) || year%400 ===0) {
         return true;
     } else {
         return false;
