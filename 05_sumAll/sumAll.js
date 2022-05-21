12274b61e95aab677c1a8e5b7c3904acd4beddb4
Abigail <abigail.uduji@gmail.com>
2022-05-20 23:13:28 -0500

    Complete exercise 04 and start 05
    get 05 working with century years

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (start < 0|| end < 0) {
+        return "ERROR";
+    } else if (isNaN(start) || isNaN(end))  {
+        return "ERROR";
+    } else if (typeof start === "string" || typeof end === "string") {
+        return "ERROR";
+    }
+    let i = Math.max(start,end);
+    let sum=0;
+    while(i>0) {
+        sum = i + sum;
+        i--;
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let result;
+    if (year % 4 === 0 ) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-};
+// if year % 4 === 0 && (!(year===100))
 
 // Do not edit below this line
 module.exports = leapYears;
