6d4b4854006480d7c8031b6ebe085eab7da48240
Abigail <abigail.uduji@gmail.com>
2022-05-20 23:30:39 -0500

    Make function round to one decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,14 +1,14 @@
 const ftoc = function(tempF) {
   let degreesC;
   degreesC = (tempF-32)*5/9;
-  return degreesC;
+  return Number(degreesC.toFixed(1));
 
 };
 
 const ctof = function(tempC) {
   let degreesF;
   degreesF = (tempC*9/5)+32
-  return degreesF;
+  return Number(degreesF.toFixed(1));
 };
 
 // Do not edit below this line
