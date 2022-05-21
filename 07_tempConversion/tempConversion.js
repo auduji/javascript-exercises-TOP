ca896226b9d6cedc84203836be9d1234cd4e063a
Abigail <abigail.uduji@gmail.com>
2022-05-20 23:25:43 -0500

    Basic function built for exercise 07
    convert ctof and ftoc

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
+const ftoc = function(tempF) {
+  let degreesC;
+  degreesC = (tempF-32)*5/9;
+  return degreesC;
 
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let degreesF;
+  degreesF = (tempC*9/5)+32
+  return degreesF;
 };
 
 // Do not edit below this line
